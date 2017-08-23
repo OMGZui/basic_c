58d660a20a706c2593835a51e7068fed789556b7
omgzui <862275679@qq.com>
2017-08-23 10:34:43 +0800

    regain

--- hanoi.c
+++ hanoi.c
@@ -3,32 +3,31 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
 void f(int n, char A, char B, char C)
 {
-	if(1 == n)
+	if (1 == n)
 	{
-		printf("编号%d从%c移到%c\n",n,A,C);
+		printf("编号%d从%c移到%c\n", n, A, C);
 	}
 	else
 	{
-		f(n-1,A,C,B);
-		printf("编号%d从%c移到%c\n",n,A,C);
-		f(n-1,B,A,C);
+		f(n - 1, A, C, B);
+		printf("编号%d从%c移到%c\n", n, A, C);
+		f(n - 1, B, A, C);
 	}
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int n;
 	char A = 'A';
 	char B = 'B';
 	char C = 'C';
-	
+
 	printf("请输入个数：");
-	scanf("%d",&n);
-	
-	f(n,'A','B','C');
-	
+	scanf("%d", &n);
+
+	f(n, 'A', 'B', 'C');
+
 	return 0;
 }
