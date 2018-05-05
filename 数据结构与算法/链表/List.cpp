be3164e2fa409203a29362039ae287b3b62d4225
omgzui <862275679@qq.com>
2018-05-05 14:01:56 +0800

    链表排序和插入

--- 数据结构与算法/链表/List.cpp
+++ 数据结构与算法/链表/List.cpp
@@ -16,17 +16,32 @@ List init();
 void show(List);
 bool is_empty(List);
 int length(List);
+void sort(List);
+bool insert(List, int, int);
 
 int main(void)
 {
     int i;
+    int pos, val;
+    // 初始化
     List pHead = init();
     if (!is_empty(pHead))
     {
         show(pHead);
     }
     printf("链表长度为：%d\n", length(pHead));
-
+    // 排序
+    // sort(pHead);
+    // show(pHead);
+    // 插入
+    printf("插入到：");
+    scanf("%d", &pos);
+    printf("值为:");
+    scanf("%d", &val);
+    if (insert(pHead, pos, val))
+    {
+        show(pHead);
+    }
 
     return 0;
 }
@@ -105,4 +120,58 @@ int length(List pHead)
         p = p->next;
     }
     return len;
+}
+
+// 排序
+void sort(List pHead)
+{
+    int len = length(pHead);
+    int i, j, t;
+    List p, q;
+    for (i = 0, p = pHead->next; i < len - 1; i++, p = p->next)
+    {
+        for (j = i + 1, q = p->next; j < len; j++, q = q->next)
+        {
+            if (p->data > q->data)
+            {
+                t = p->data;
+                p->data = q->data;
+                q->data = t;
+            }
+        }
+    }
+    return;
+}
+
+// 插入
+bool insert(List pHead, int pos, int val)
+{
+    int i;
+
+    List p = pHead;
+
+    // 拿到第pos个位置的地址
+    while (NULL != p && i < pos - 1)
+    {
+        p = p->next;
+        i++;
+    }
+
+    if (i > pos - 1 || p == NULL)
+    {
+        printf("位置不符\n");
+        return false;
+    }
+
+    // 需要插入的新节点
+    List pNew = (List)malloc(sizeof(LNode));
+    pNew->data = val;
+    // pos+1位置地址
+    List q = p->next;
+    // pos+1位置指向了pNew
+    p->next = pNew;
+    // pNew指向了之前的pos+1位置地址
+    pNew->next = q;
+
+    return true;
 }
