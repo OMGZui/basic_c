8ef427f9a41f50d0d3c8f6eaa358945e8e94dee8
omgzui <862275679@qq.com>
2018-05-28 16:38:37 +0800

    冒泡排序

---
+++ 数据结构与算法/10排序/bubble.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/malloc.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define Len(array) ((sizeof(array)) / (sizeof(array[0])))
+
+void bubble(int *, int);
+void printArr(int *, int);
+
+int main(int argc, char **agrv)
+{
+    int arr[] = {10, -20, 5, 99, -49, 49};
+    int len = Len(arr);
+    printf("排序前：");
+    printArr(arr, len);
+
+    bubble(arr, len);
+    printf("排序后：");
+    printArr(arr, len);
+    return 0;
+}
+
+// 冒泡排序
+void bubble(int *arr, int len)
+{
+    int i, j, temp;
+    for (i = 0; i < len; i++)
+    {
+        for (j = i + 1; j < len; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
+// 输出
+void printArr(int *arr, int len)
+{
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+}
